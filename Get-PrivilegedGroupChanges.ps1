79463ed921f2d6c75e0879877906f77f070193e8
0xFFF0007 <JeskeCode@users.noreply.github.com>
2015-02-17 11:26:15 -0500

    Create Get-PrivilegedGroupChanges.ps1

---
+++ Get-PrivilegedGroupChanges.ps1
@@ -0,0 +1,36 @@
+Function Get-PrivilegedGroupChanges {            
+Param(            
+    $Server = (Get-ADDomainController -Discover | Select-Object -ExpandProperty HostName),            
+    $Hour = 24            
+)            
+            
+    $ProtectedGroups = Get-ADGroup -Filter 'AdminCount -eq 1' -Server $Server            
+    $Members = @()            
+            
+    ForEach ($Group in $ProtectedGroups) {            
+        $Members += Get-ADReplicationAttributeMetadata -Server $Server `
+            -Object $Group.DistinguishedName -ShowAllLinkedValues |            
+         Where-Object {$_.IsLinkValue} |            
+         Select-Object @{name='GroupDN';expression={$Group.DistinguishedName}}, `
+            @{name='GroupName';expression={$Group.Name}}, *            
+    }            
+            
+    $Members |            
+        Where-Object {$_.LastOriginatingChangeTime -gt (Get-Date).AddHours(-1 * $Hour)}            
+            
+}            
+
+# Get-ADUser -Filter 'AdminCount -eq 1'
+# Get-ADGroup -Filter 'AdminCount -eq 1'            
+            
+# Last 24 hours            
+Get-PrivilegedGroupChanges            
+            
+# Last week            
+# Get-PrivilegedGroupChanges -Hour (7*24)| Out-Gridview           
+            
+# Specific DC, 30 days (24 hours x 30 days)            
+# Get-PrivilegedGroupChanges -Server CVDC1.contoso.com -Hour (24*30)            
+            
+# Last year of group changes            
+# Get-PrivilegedGroupChanges -Hour (365*24) | Export-Csv -Path .\PrivGroupMembershipChanges.csv -NoTypeInformation  
